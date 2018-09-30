dc7d5c015c81028486f9fedad151a346a508c2c2
chrisrbugs <43486140+chrisrbugs@users.noreply.github.com>
2018-09-30 11:48:06 -0300

    Add files via upload

---
+++ app/model/Album.class.php
@@ -0,0 +1,19 @@
+<?php
+class Album extends TRecord
+{
+    const TABLENAME  = 'album';
+    const PRIMARYKEY = 'id';
+    const IDPOLICY   =  'serial'; // {max, serial}
+    
+    
+    
+    /**
+     * Constructor method
+     */
+    public function __construct($id = NULL, $callObjectLoad = TRUE)
+    {
+        parent::__construct($id, $callObjectLoad);
+        parent::addAttribute('descricao');
+    }
+}
+?>
