7c8391d70d991e51c06c8651a30904f224a5c815
chrisrbugs <43486140+chrisrbugs@users.noreply.github.com>
2018-09-30 13:12:52 -0300

    Update AlbumForm.class.php

--- app/control/cadastros/AlbumForm.class.php
+++ app/control/cadastros/AlbumForm.class.php
@@ -95,13 +95,15 @@ class AlbumForm extends TPage
 		    
 		$dados_file = json_decode(urldecode($foto));
 		$foto = explode('/',$dados_file->fileName)[1];
-		
-		// copy file to target folder
-		$this->saveFilesByComma($fotos_album, $data, 'photo_path', 'album');
 		    
 		$fotos_album->caminho_foto = "album/".$foto;
 		$fotos_album->ref_album = $album->id;
 		$fotos_album->store();
+		
+		// copy file to target folder
+		$this->saveFilesByComma($fotos_album, $data, 'photo_path', 'album');
+		    
+		
 		
                 
 		
